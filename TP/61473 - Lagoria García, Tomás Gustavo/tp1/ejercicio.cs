81da1d6550933ada3b8489c1e2e4ec6cb9ab6745
tomaslago27 <116082698+tomaslago27@users.noreply.github.com>
2025-03-27 20:04:11 -0300

    Update ejercicio.cs
    Primeras líneas de código. Probando el guardado en el repositorio

--- TP/61473 - Lagoria García, Tomás Gustavo/tp1/ejercicio.cs
+++ TP/61473 - Lagoria García, Tomás Gustavo/tp1/ejercicio.cs
@@ -12,6 +12,62 @@ using System.IO;    // Para leer archivos    (File)
 // File.WriteLines(destino, lineas) : Escribe una lista de líneas en un archivo
 
 // Escribir la solucion al TP1 en este archivo. (Borre el ejemplo de abajo)
+struct Contacto{
+    public string ID;
+    public string Nombre;
+    public string Telefono;
+    public string Email;
+
+    public Contacto(string id, string nombre, string telefono, string email){
+        ID = id;
+        Nombre = nombre;
+        Telefono = telefono;
+        Email = email;
+    }
+}
+struct Agenda{
+    static public int CantidadTotal=10;
+    public Contacto[] Contactos;
+    public Agenda(){
+        Contactos = new Contacto[CantidadTotal];
+        var CantidadContactos = 0;
+    }
+    public void AgregarContacto(Contacto contac){
+        if(CantidadContactos < CantidadTotal){
+            Contactos[CantidadContactos] = contac;
+            CantidadContactos++;
+            if(CantidadContactos == CantidadTotal-1){
+                Console.WriteLine("La agenda le queda un lugar disponible de memoria");
+            }
+        }
+          Console.WriteLine("La agenda está llena");
+    }
+    public void MostrarContactos(){
+        for(int i = 0; i < CantidadContactos; i++){
+            Console.WriteLine("ID: " + Contactos[i].ID);
+            Console.WriteLine("Nombre: " + Contactos[i].Nombre);
+            Console.WriteLine("Telefono: " + Contactos[i].Telefono);
+            Console.WriteLine("Email: " + Contactos[i].Email);
+            Console.WriteLine();
+        }
+    }
+    public void GuardarContactos(string archivo){
+        string[] lineas = new string[CantidadContactos];
+        for(int i = 0; i < CantidadContactos; i++){
+            lineas[i] = Contactos[i].ID + ";" + Contactos[i].Nombre + ";" + Contactos[i].Telefono + ";" + Contactos[i].Email;
+        }
+        File.WriteAllLines(archivo, lineas);
+    }
+    public void CargarContactos(string archivo){
+        string[] lineas = File.ReadAllLines(archivo);
+        for(int i = 0; i < lineas.Length; i++){
+            string[] campos = lineas[i].Split(';');
+            Contacto contacto = new Contacto(campos[0], campos[1], campos[2], campos[3]);
+            AgregarContacto(contacto);
+        }
+    }
+}
 Console.WriteLine("Hola, soy el ejercicio 1 del TP1 de la materia Programación 3");
 Console.Write("Presionar una tecla para continuar...");
 Console.ReadKey();
+Console.Clear();
