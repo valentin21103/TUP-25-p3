e0da404ba928ba645831e3d6fad077abd4c274a4
Tomtestt <142940242+Tomtestt@users.noreply.github.com>
2025-04-05 17:11:43 -0300

    Update ejercicio.cs

--- TP/61290 - Azubel, Tomas/TP2/ejercicio.cs
+++ TP/61290 - Azubel, Tomas/TP2/ejercicio.cs
@@ -1,60 +1,255 @@
-// TP2: Sistema de Cuentas Bancarias
-//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public abstract class Operation
+{
+    public double Amount { get; }
+    public string SourceAccount { get; }
+    public string SourceClient { get; }
 
-// Implementar un sistema de cuentas bancarias que permita realizar operaciones como depósitos, retiros, transferencias y pagos.
+    protected Operation(double amount, string sourceAccount, string sourceClient)
+    {
+        Amount = amount;
+        SourceAccount = sourceAccount;
+        SourceClient = sourceClient;
+    }
 
-class Banco{}
-class Cliente{}
+    public abstract void Execute(Bank bank);
+}
+public class Deposit : Operation
+{
+    public Deposit(string sourceAccount, double amount) : base(amount, sourceAccount, "") { }
 
-abstract class Cuenta{}
-class CuentaOro: Cuenta{}
-class CuentaPlata: Cuenta{}
-class CuentaBronce: Cuenta{}
+    public override void Execute(Bank bank)
+    {
+        var account = bank.FindAccount(SourceAccount);
+        if (account != null)
+        {
+            account.Balance += Amount;
+            account.History.Add($"Depósito ${Amount} a [{SourceAccount}/{account.Client.Name}]");
+        }
+    }
+}
+public class Withdrawal : Operation
+{
+    public Withdrawal(string sourceAccount, double amount) : base(amount, sourceAccount, "") { }
 
-abstract class Operacion{}
-class Deposito: Operacion{}
-class Retiro: Operacion{}
-class Transferencia: Operacion{}
-class Pago: Operacion{}
+    public override void Execute(Bank bank)
+    {
+        var account = bank.FindAccount(SourceAccount);
+        if (account != null && account.Balance >= Amount)
+        {
+            account.Balance -= Amount;
+            account.History.Add($"Retiro ${Amount} de [{SourceAccount}/{account.Client.Name}]");
+        }
+        else
+        {
+            Console.WriteLine($"Fondos insuficientes en la cuenta {SourceAccount}");
+        }
+    }
+}
+public class Payment : Operation
+{
+    public Payment(string sourceAccount, double amount) : base(amount, sourceAccount, "") { }
 
+    public override void Execute(Bank bank)
+    {
+        var account = bank.FindAccount(SourceAccount);
+        if (account != null && account.Balance >= Amount)
+        {
+            account.Balance -= Amount;
+            account.AccumulatePoints(Amount);
+            account.History.Add($"Pago ${Amount} con [{SourceAccount}/{account.Client.Name}]");
+        }
+        else
+        {
+            Console.WriteLine($"Fondos insuficientes en la cuenta {SourceAccount}");
+        }
+    }
+}
+public class Transfer : Operation
+{
+    public string DestinationAccount { get; }
 
-/// EJEMPLO DE USO ///
+    public Transfer(string sourceAccount, string destinationAccount, double amount) : base(amount, sourceAccount, "") 
+    {
+        DestinationAccount = destinationAccount;
+    }
 
-// Definiciones 
+    public override void Execute(Bank bank)
+    {
+        var sourceAccount = bank.FindAccount(SourceAccount);
+        var destinationAccount = bank.FindAccount(DestinationAccount);
 
-var raul = new Cliente("Raul Perez");
-    raul.Agregar(new CuentaOro("10001", 1000));
-    raul.Agregar(new CuentaPlata("10002", 2000));
+        if (sourceAccount != null && destinationAccount != null && sourceAccount.Balance >= Amount)
+        {
+            sourceAccount.Balance -= Amount;
+            destinationAccount.Balance += Amount;
+            sourceAccount.History.Add($"Transferencia ${Amount} de [{SourceAccount}/{sourceAccount.Client.Name}] a [{DestinationAccount}/{destinationAccount.Client.Name}]");
+            destinationAccount.History.Add($"Transferencia ${Amount} de [{SourceAccount}/{sourceAccount.Client.Name}] a [{DestinationAccount}/{destinationAccount.Client.Name}]");
+        }
+        else
+        {
+            Console.WriteLine($"Fondos insuficientes o cuenta de destino no encontrada en la transferencia de [{SourceAccount}] a [{DestinationAccount}]");
+        }
+    }
+}
+public abstract class Account
+{
+    public string Number { get; }
+    public double Balance { get; set; }
+    public Client Client { get; }
+    public List<string> History { get; }
+    private double _points;
 
-var sara = new Cliente("Sara Lopez");
-    sara.Agregar(new CuentaPlata("10003", 3000));
-    sara.Agregar(new CuentaPlata("10004", 4000));
+    public double Points 
+    {
+        get { return _points; }
+        set { _points = value; }
+    }
 
-var luis = new Cliente("Luis Gomez");
-    luis.Agregar(new CuentaBronce("10005", 5000));
+    protected Account(string number, double balance, Client client)
+    {
+        Number = number;
+        Balance = balance;
+        Client = client;
+        History = new List<string>();
+    }
 
-var nac = new Banco("Banco Nac");
-nac.Agregar(raul);
-nac.Agregar(sara);
+    public abstract void AccumulatePoints(double amount);
+}
+public class GoldAccount : Account
+{
+    public GoldAccount(string number, double balance, Client client) : base(number, balance, client) { }
 
-var tup = new Banco("Banco TUP");
-tup.Agregar(luis);
+    public override void AccumulatePoints(double amount)
+    {
+        double percentage = amount > 1000 ? 0.05 : 0.03;
+        Points += amount * percentage;
+    }
+}
 
+public class SilverAccount : Account
+{
+    public SilverAccount(string number, double balance, Client client) : base(number, balance, client) { }
 
-// Registrar Operaciones
-nac.Registrar(new Deposito("10001", 100));
-nac.Registrar(new Retiro("10002", 200));
-nac.Registrar(new Transferencia("10001", "10002", 300));
-nac.Registrar(new Transferencia("10003", "10004", 500));
-nac.Registrar(new Pago("10002", 400));
+    public override void AccumulatePoints(double amount)
+    {
+        Points += amount * 0.02;
+    }
+}
 
-tup.Registrar(new Deposito("10005", 100));
-tup.Registrar(new Retiro("10005", 200));
-tup.Registrar(new Transferencia("10005", "10002", 300));
-tup.Registrar(new Pago("10005", 400));
+public class BronzeAccount : Account
+{
+    public BronzeAccount(string number, double balance, Client client) : base(number, balance, client) { }
 
+    public override void AccumulatePoints(double amount)
+    {
+        Points += amount * 0.01;
+    }
+}
+public class Client
+{
+    public string Name { get; }
+    public List<Account> Accounts { get; }
+    public Bank Bank { get; set; }
 
-// Informe final
-nac.Informe();
-tup.Informe();
+    public Client(string name)
+    {
+        Name = name;
+        Accounts = new List<Account>();
+    }
 
+    public void Add(Account account)
+    {
+        Accounts.Add(account);
+    }
+}
+public class Bank
+{
+    public string Name { get; }
+    private List<Client> Clients { get; }
+    private List<Operation> Operations { get; }
+
+    public Bank(string name)
+    {
+        Name = name;
+        Clients = new List<Client>();
+        Operations = new List<Operation>();
+    }
+
+    public void Add(Client client)
+    {
+        Clients.Add(client);
+        client.Bank = this;
+    }
+
+    public Account FindAccount(string accountNumber)
+    {
+        foreach (var client in Clients)
+        {
+            var account = client.Accounts.FirstOrDefault(a => a.Number == accountNumber);
+            if (account != null)
+                return account;
+        }
+        return null;
+    }
+
+    public void Register(Operation operation)
+    {
+        Operations.Add(operation);
+        operation.Execute(this);
+    }
+
+    public void Report()
+    {
+        Console.WriteLine($"Banco: {Name} | Clientes: {Clients.Count}");
+        foreach (var client in Clients)
+        {
+            double totalBalance = client.Accounts.Sum(a => a.Balance);
+            double totalPoints = client.Accounts.Sum(a => a.Points);
+
+            Console.WriteLine($"\n  Cliente: {client.Name} | Saldo Total: $ {totalBalance:F2} | Puntos Total: $ {totalPoints:F2}");
+            foreach (var account in client.Accounts)
+            {
+                Console.WriteLine($"    Cuenta: {account.Number} | Saldo: $ {account.Balance:F2} | Puntos: $ {account.Points:F2}");
+                foreach (var operation in account.History)
+                {
+                    Console.WriteLine($"     -  {operation}");
+                }
+            }
+        }
+    }
+}
+
+var raul = new Client("Raul Perez");
+raul.Add(new GoldAccount("10001", 1000, raul));
+raul.Add(new SilverAccount("10002", 2000, raul));
+
+var sara = new Client("Sara Lopez");
+sara.Add(new SilverAccount("10003", 3000, sara));
+sara.Add(new SilverAccount("10004", 4000, sara));
+
+var luis = new Client("Luis Gomez");
+luis.Add(new BronzeAccount("10005", 5000, luis));
+
+var nac = new Bank("Banco Nac");
+nac.Add(raul);
+nac.Add(sara);
+
+var tup = new Bank("Banco TUP");
+tup.Add(luis);
+
+nac.Register(new Deposit("10001", 100));
+nac.Register(new Withdrawal("10002", 200));
+nac.Register(new Transfer("10001", "10002", 300));
+nac.Register(new Transfer("10003", "10004", 500));
+nac.Register(new Payment("10002", 400));
+
+tup.Register(new Deposit("10005", 100));
+tup.Register(new Withdrawal("10005", 200));
+tup.Register(new Transfer("10005", "10002", 300));
+tup.Register(new Payment("10005", 400));
+
+nac.Report();
+tup.Report();
