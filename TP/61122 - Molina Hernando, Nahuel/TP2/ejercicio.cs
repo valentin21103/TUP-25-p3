d30b556e1dffb89e83fdff985e72cf829f4ba156
Nahumolher <nahuelmolher024@gmail.com>
2025-04-05 18:57:01 -0300

    Update ejercicio.cs
    ejercicio corregido

--- TP/61122 - Molina Hernando, Nahuel/TP2/ejercicio.cs
+++ TP/61122 - Molina Hernando, Nahuel/TP2/ejercicio.cs
@@ -1,10 +1,9 @@
-/using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace SistemaBancario
 {
-    
     abstract class Cuenta
     {
         public string Numero { get; }
@@ -23,6 +22,7 @@ namespace SistemaBancario
         public void Depositar(decimal monto)
         {
             Saldo += monto;
+            AcumularPuntos(monto);
         }
 
         public bool Extraer(decimal monto)
@@ -35,7 +35,6 @@ namespace SistemaBancario
             return false;
         }
     }
-
 
     class CuentaOro : Cuenta
     {
@@ -66,7 +65,7 @@ namespace SistemaBancario
             Puntos += monto * 0.01m;
         }
     }
-    
+
     class Cliente
     {
         public string Nombre { get; }
@@ -126,7 +125,45 @@ namespace SistemaBancario
         }
     }
 
-    
+    class Transferencia : Operacion
+    {
+        public string CuentaDestino { get; }
+
+        public Transferencia(string cuentaOrigen, string cuentaDestino, decimal monto) : base(cuentaOrigen, monto)
+        {
+            CuentaDestino = cuentaDestino;
+        }
+
+        public override void Ejecutar(Banco banco)
+        {
+            var cuentaOrigen = banco.ObtenerCuenta(CuentaOrigen);
+            var cuentaDestino = banco.ObtenerCuenta(CuentaDestino);
+
+            if (cuentaOrigen != null && cuentaDestino != null && cuentaOrigen.Extraer(Monto))
+            {
+                cuentaDestino.Depositar(Monto);
+            }
+            else
+            {
+                Console.WriteLine($"No se pudo realizar la transferencia de {CuentaOrigen} a {CuentaDestino}.");
+            }
+        }
+    }
+
+    class Pago : Operacion
+    {
+        public Pago(string cuentaOrigen, decimal monto) : base(cuentaOrigen, monto) { }
+
+        public override void Ejecutar(Banco banco)
+        {
+            var cuenta = banco.ObtenerCuenta(CuentaOrigen);
+            if (cuenta != null && !cuenta.Extraer(Monto))
+            {
+                Console.WriteLine($"Fondos insuficientes para realizar el pago desde la cuenta {CuentaOrigen}.");
+            }
+        }
+    }
+
     class Banco
     {
         public string Nombre { get; }
@@ -174,19 +211,40 @@ namespace SistemaBancario
     {
         static void Main(string[] args)
         {
-            // Ejemplo de uso
+            
             var raul = new Cliente("Raul Perez");
             raul.AgregarCuenta(new CuentaOro("10001", 1000));
             raul.AgregarCuenta(new CuentaPlata("10002", 2000));
 
+            var sara = new Cliente("Sara Lopez");
+            sara.AgregarCuenta(new CuentaPlata("10003", 3000));
+            sara.AgregarCuenta(new CuentaPlata("10004", 4000));
+
+            var luis = new Cliente("Luis Gomez");
+            luis.AgregarCuenta(new CuentaBronce("10005", 5000));
+
+            var nac = new Banco("Banco Nac");
+            nac.AgregarCliente(raul);
+            nac.AgregarCliente(sara);
+
+            var tup = new Banco("Banco TUP");
+            tup.AgregarCliente(luis);
+
             
-            var banco = new Banco("Banco UTN");
-            banco.AgregarCliente(raul);
+            nac.RegistrarOperacion(new Deposito("10001", 100));
+            nac.RegistrarOperacion(new Retiro("10002", 200));
+            nac.RegistrarOperacion(new Transferencia("10001", "10002", 300));
+            nac.RegistrarOperacion(new Transferencia("10003", "10004", 500));
+            nac.RegistrarOperacion(new Pago("10002", 400));
 
-            banco.RegistrarOperacion(new Deposito("10001", 500));
-            banco.RegistrarOperacion(new Retiro("10002", 300));
+            tup.RegistrarOperacion(new Deposito("10005", 100));
+            tup.RegistrarOperacion(new Retiro("10005", 200));
+            tup.RegistrarOperacion(new Transferencia("10005", "10002", 300));
+            tup.RegistrarOperacion(new Pago("10005", 400));
 
-            banco.Informe();
+           
+            nac.Informe();
+            tup.Informe();
         }
     }
 }
