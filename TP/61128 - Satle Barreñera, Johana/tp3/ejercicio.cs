20c8afe501ba8f20e75d0d26db3159efe0463f40
Joosatle <barrenadajohana@gmail.com>
2025-04-12 09:53:16 -0300

    Update ejercicio.cs (#245)

--- TP/61128 - Satle Barreñera, Johana/tp3/ejercicio.cs
+++ TP/61128 - Satle Barreñera, Johana/tp3/ejercicio.cs
@@ -1,15 +1,82 @@
 using System;
 using System.Collections.Generic;
 
+class ListaOrdenada<T> where T : IComparable<T>
+{
+    private List<T> elementos = new List<T>();
 
-class ListaOrdenada{
-    // Implementar acá la clase ListaOrdenada
+    public ListaOrdenada() { }
+
+    public ListaOrdenada(IEnumerable<T> coleccion)
+    {
+        foreach (var item in coleccion)
+        {
+            Agregar(item);
+        }
+    }
+
+    public void Agregar(T elemento)
+    {
+        if (Contiene(elemento)) return;
+
+        int index = elementos.BinarySearch(elemento);
+        if (index < 0) index = ~index;
+        elementos.Insert(index, elemento);
+    }
+
+    public bool Contiene(T elemento)
+    {
+        return elementos.Contains(elemento);
+    }
+
+    public void Eliminar(T elemento)
+    {
+        elementos.Remove(elemento);
+    }
+
+    public int Cantidad => elementos.Count;
+
+    public T this[int indice] => elementos[indice];
+
+    public ListaOrdenada<T> Filtrar(Predicate<T> condicion)
+    {
+        var resultado = new ListaOrdenada<T>();
+        foreach (var item in elementos)
+        {
+            if (condicion(item))
+                resultado.Agregar(item);
+        }
+        return resultado;
+    }
 }
 
-class Contacto {
+class Contacto : IComparable<Contacto>
+{
     public string Nombre { get; set; }
     public string Telefono { get; set; }
-    // Implementar acá la clase Contacto
+
+    public Contacto(string nombre, string telefono)
+    {
+        Nombre = nombre;
+        Telefono = telefono;
+    }
+
+    public int CompareTo(Contacto otro)
+    {
+        return this.Nombre.CompareTo(otro.Nombre);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Contacto otro)
+            return this.Nombre == otro.Nombre && this.Telefono == otro.Telefono;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return Nombre.GetHashCode() + Telefono.GetHashCode();
+    }
 }
 
 /// --------------------------------------------------------///
